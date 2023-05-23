714378d95443ec2604378d187162e2aac21f7a83
Shanre Scheepers <21100387@virtualwindow.co.za>
2023-05-24 00:01:04 +0200

    : ft navigation phase : tab bar

--- WildMoments/navigators/HomeTab.js
+++ WildMoments/navigators/HomeTab.js
@@ -1,17 +1,76 @@
 // Tab Bar for Home Screens
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Image } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import HomeScreen from '../screens/UserJourney/HomeScreen';
+import CompsScreen from '../screens/UserJourney/CompsScreen';
+import GalleryScreen from '../screens/UserJourney/GalleryScreen';
+import ExploreScreen from '../screens/UserJourney/ExploreScreen';
+import { RFPercentage } from 'react-native-responsive-fontsize';
 
-
+const Tab = createBottomTabNavigator();
 
 const HomeTab = () => {
     return (
-        <View>
-            <Text>HomeTab</Text>
-        </View>
+        <Tab.Navigator style={styles.tabbar}
+            screenOptions={{
+                tabBarActiveTintColor: "#FA993B",
+                tabBarStyle: { backgroundColor: '#202022', height: 90, borderTopLeftRadius: 35, borderTopRightRadius: 35, padding: 8 }
+            }} >
+
+            <Tab.Screen name='HomeScreen' component={HomeScreen}
+                options={({ focused, size }) =>
+                ({
+                    tabBarIcon: ({ size, tabInfo }) => (
+                        <Image
+                            style={{ width: 30, height: 30 }}
+                            source={require('../assets/AppIcons/homeIcon.png')}
+                        />
+                    ),
+                })}
+            />
+
+            <Tab.Screen name='CompsScreen' component={CompsScreen} options={({ focused, size }) =>
+            ({
+                tabBarIcon: ({ size, tabInfo }) => (
+                    <Image
+                        style={{ width: size, height: size }}
+                        source={require('../assets/AppIcons/compsIcon.png')}
+                    />
+                ),
+
+            })} />
+
+            <Tab.Screen name='ExploreScreen' component={ExploreScreen} options={({ focused, size }) =>
+            ({
+                tabBarIcon: ({ size, tabInfo }) => (
+                    <Image
+                        style={{ width: size, height: size }}
+                        source={require('../assets/AppIcons/exploreIcon.png')}
+                    />
+                ),
+
+            })} />
+
+            <Tab.Screen name='GalleryScreen' component={GalleryScreen} options={({ focused, size }) =>
+            ({
+                tabBarIcon: ({ size, tabInfo }) => (
+                    <Image
+                        style={{ width: size, height: size }}
+                        source={require('../assets/AppIcons/galleryIcon.png')}
+                    />
+                ),
+
+            })} />
+        </Tab.Navigator>
     )
 }
 
 export default HomeTab
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    tabbar: {
+        height: RFPercentage(50),
+    },
+})
